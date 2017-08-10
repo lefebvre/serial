d9fae36a12e59d56a4f807145bc606a67b3f90a5
Jordan P. Lefebvre <lefebvrejp@ornl.gov>
2017-08-10 17:13:25 -0400

    Initial TriBIT packaging.

---
+++ ProjectName.cmake
@@ -0,0 +1,12 @@
+##---------------------------------------------------------------------------##
+## serial/ProjectName.cmake
+## Jordan Lefebvre, lefebvre.jordan@gmail.com
+##---------------------------------------------------------------------------##
+
+# Must set the project name as a variable at very beginning before including
+# anything else
+SET(PROJECT_NAME serial)
+
+##---------------------------------------------------------------------------##
+##                    end of serial/ProjectName.cmake
+##---------------------------------------------------------------------------##
